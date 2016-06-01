f85d715756c2549a24877bc2b835caf25d168864
Samuel J. Greear <sjg@thesjg.com>
2016-06-01 09:41:14 -0600

    Only print analytics code if user is not logged in as an admin

--- footer.php
+++ footer.php
@@ -4,19 +4,21 @@
     wp_footer();
     echo origin_get_setting('origin_theme_injection_bodyclose');
 
-    $gacode = origin_get_setting('origin_theme_analytics_gacode');
-    if (!empty($gacode)) {
-        ?>
-        <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    if (!current_user_can('manage_options')) {
+        $gacode = origin_get_setting('origin_theme_analytics_gacode');
+        if (!empty($gacode)) {
+            ?>
+            <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-        ga('create', '<?php echo $gacode; ?>', 'auto');
-        ga('send', 'pageview');
-        </script>
-        <?php
+            ga('create', '<?php echo $gacode; ?>', 'auto');
+            ga('send', 'pageview');
+            </script>
+            <?php
+        }
     }
 ?>
 </body>
