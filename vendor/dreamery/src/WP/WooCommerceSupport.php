859cd05b59e9853994f05a2f619ff6ea15b846c3
Samuel J. Greear <sjg@thesjg.com>
2016-09-13 16:40:39 -0600

    Fix typo in callback name

--- vendor/dreamery/src/WP/WooCommerceSupport.php
+++ vendor/dreamery/src/WP/WooCommerceSupport.php
@@ -8,7 +8,7 @@ class WooCommerceSupport {
         remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
         add_action('woocommerce_before_main_content', array($this, 'my_theme_wrapper_start'), 10);
         add_action('woocommerce_after_main_content', array($this, 'my_theme_wrapper_end'), 10);
-        add_action('after_setup_theme', array($this, 'woocommerce_support'));
+        add_action('after_setup_theme', array($this, 'declare_support'));
     }
 
     /*
