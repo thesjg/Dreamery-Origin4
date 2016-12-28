aa9bbb4632e162464a48e0db31d9072c35ebafa1
Samuel J. Greear <sjg@thesjg.com>
2016-12-28 16:46:50 -0700

    Start stubbing out footer

--- partials/page-footer.php
+++ partials/page-footer.php
@@ -1,5 +1,25 @@
 <footer>
     <div class="container">
-        <h2>Footer</h2>
+        <div class="row">
+            <div class="col-md-4">
+                <?php
+                $logo = '';
+                if (function_exists('the_custom_logo')) {
+                    $logo = the_custom_logo();
+                }
+                if (empty($logo)) {
+                    echo '<a href="/"><img src="' . get_template_directory_uri() . '/assets/img/logo.png"></a>';
+                } else {
+                    echo $logo;
+                }
+                ?>
+            </div>
+            <div class="col-md-4">
+                Widget
+            </div>
+            <div class="col-md-4">
+                Widget
+            </div>
+        </div>
     </div>
 </footer>
